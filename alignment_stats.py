ed715af62760c8072dfb5923cf85096bd575c33e
Richard Linchangco <rlinch2@gmail.com>
2021-11-21 23:45:35 -0600

    substitutions and positions

--- alignment_stats.py
+++ alignment_stats.py
@@ -96,6 +96,115 @@ def compare_seqs(seq1,seq2,distance_metric,genome_ref_seq=None,read_frame=0):
         return insertion_count,deletion_count,substitution_count,total_length,distance,distance_var
 
 
+def compare_seqs_by_codon(seq1,seq2,distance_metric,genome_ref_seq=None,read_frame=0):
+    """
+    """
+    # print(len(genome_ref_seq),genome_ref_seq.count('-'))          # expectation of 9719 positions for genome_ref_seq
+    genome_ref_loc = []                                             # genome reference location list to store substitutions positions
+    insertion_count = 0
+    deletion_count = 0
+    substitution_count = 0
+    length = len(seq1)
+    if length != len(seq2):
+        print("NOT SAME LENGTH")
+        length = [len(seq1),len(seq2)]
+    apreceding = 0
+    atrailing = 0
+    bpreceding = 0
+    btrailing = 0    
+    # account for preceding or trailing "gaps"    
+    if seq1.startswith('-'):
+        apreceding = len(seq1) - len(seq1.lstrip('-'))
+    if seq1.endswith('-'):
+        atrailing = len(seq1) - len(seq1.rstrip('-'))
+    if seq2.startswith('-'):
+        bpreceding = len(seq2) - len(seq2.lstrip('-'))
+    if seq2.endswith('-'):
+        btrailing = len(seq2) - len(seq2.rstrip('-'))
+    ref_loc = 0
+    for i, (a, b) in enumerate(zip(seq1, seq2)):
+        if genome_ref_seq:
+            if genome_ref_seq[i] != '-':
+                a = a.lower()
+                b = b.lower()
+                if a != b:                                                  # bases are different
+                    if a == '-':
+                        insertion_count += 1
+                    elif b == '-':
+                        deletion_count += 1
+                    else:
+                        substitution_count += 1
+                        genome_ref_loc.append(ref_loc+read_frame)
+                ref_loc += 1                                                # increment ref_loc by a position
+        else:
+            a = a.lower()
+            b = b.lower()
+            if a != b:                                                  # bases are different
+                if a == '-':
+                    insertion_count += 1
+                elif b == '-':
+                    deletion_count += 1
+                else:
+                    substitution_count += 1
+    print(seq1.count('-'),apreceding+atrailing,insertion_count)
+    insertion_count = insertion_count - apreceding - atrailing
+    # print(deletion_count,bpreceding,btrailing)
+    deletion_count = deletion_count - bpreceding - btrailing
+    total_length = length - apreceding - atrailing - bpreceding - btrailing
+    distance = None
+    if distance_metric == 'percent':
+        distance = insertion_count+deletion_count+substitution_count/total_length
+    elif distance_metric == 'hamming':
+        distance = insertion_count+deletion_count+substitution_count
+    else:
+        print("NO DISTANCE SPECIFIED, DEFAULTING TO HAMMING")
+        distance = insertion_count+deletion_count+substitution_count
+    distance_var = distance*(total_length - distance)/total_length
+    if genome_ref_seq:
+        return insertion_count,deletion_count,substitution_count,total_length,distance,distance_var,genome_ref_loc
+    else:
+        return insertion_count,deletion_count,substitution_count,total_length,distance,distance_var
+
+
+def codon_subs(fasta1,fasta2,genome_ref_seq=None):
+    """
+    finds substitutions only between old sequence and new
+    returns dataframe of: alignment index, genome position, codon position
+    """
+    sub_list = []
+    substitution_count = 0
+    genome_ref_sans_gap = 0                                     # genome reference base pair position (ignores insertions)
+    seq1 = fasta1[1]
+    seq2 = fasta2[1]
+    for i, (a, b) in enumerate(zip(seq1, seq2)):
+        sub = 'no'
+        pos = 0
+        if genome_ref_seq[i] != '-':
+            a = a.lower()
+            b = b.lower()
+            genome_ref_sans_gap += 1
+            if a != b:                                          # bases are different
+                if a == '-':                                    # insertion
+                    continue
+                elif b == '-':                                  # deletion
+                    continue
+                else:                                           # substitution
+                    substitution_count += 1
+                    sub = 'yes'
+                    if genome_ref_sans_gap % 3 == 0:            # no remainder means codon position 3
+                        pos = 3
+                    else:
+                        div = genome_ref_sans_gap / 3
+                        if str(div).endswith('7'):              # .66667 means 2/3, codon position 2
+                            pos = 2
+                        else:                                   # .33333 means 1/3, codon position 1
+                            pos = 1
+                    sub_list.append([i+1,genome_ref_sans_gap,pos])  # only add substitutions to list output
+    sub_list.insert(0,[f"query:{fasta2[0]}",f"ref:{fasta1[0]}",f"TOTAL SUBS:{substitution_count}"])
+    columns = ["alignment_index", "hxb2_genome_position", "codon_position"]
+    return pd.DataFrame(sub_list,columns=columns)
+
+
 def plot_histo(x,outPath,col):
     """
     """
@@ -173,8 +282,8 @@ def fasta_stats(file_list,outPath,distance_metric,qualifier=None):
                 seq_id = f"!{seq_id}"
             this_fasta.append((seq_id,seq))
         this_fasta.sort()                           # sort for order
-        insertion_count,deletion_count,substitution_count,length,distance_ham,distance_var,genome_ref_locs = compare_seqs(this_fasta[0][1],this_fasta[1][1],distance_metric)
-        df.loc[df_row] = [this_fasta[1][0],this_fasta[0][0],insertion_count,deletion_count,substitution_count,length,distance_ham,distance_var,genome_ref_locs]
+        insertion_count,deletion_count,substitution_count,length,distance_ham,distance_var = compare_seqs(this_fasta[0][1],this_fasta[1][1],distance_metric)
+        df.loc[df_row] = [this_fasta[1][0],this_fasta[0][0],insertion_count,deletion_count,substitution_count,length,distance_ham,distance_var]
         df_row += 1
         consensus_id = this_fasta[0][0][1:]
     for col in df.columns.to_list():
@@ -194,11 +303,11 @@ def fasta_stats(file_list,outPath,distance_metric,qualifier=None):
     # df.to_excel(df_xlsx,index=False)      # need excelwriter like openpyxl 
 
 
-def orf_stats(file_list,outPath,distance_metric):
+def codon_stats(file_list,outPath):
     """
+    only finds substitutions
     """
-    df_row = 0
-    df = pd.DataFrame(columns=['Query_ID','Mapped_to_ID','Insertions','Deletions','Substitutions','Sequence_Length','Hamming_Distance','Distance Variance','Substitution Ref position'])    
+    overall_dfs = []
     for fasta_file in file_list:
         this_fasta = []
         for seq_id,seq in readFasta(fasta_file):
@@ -206,21 +315,13 @@ def orf_stats(file_list,outPath,distance_metric):
                 seq_id = f"!REF_{seq_id}"
             this_fasta.append((seq_id,seq))
         this_fasta.sort()                           # sort for order (older sequence should be second index)
-        for i in [0,1,2,3]:                               # reading frames 0,1,2,3
-            insertion_count,deletion_count,substitution_count,length,distance_ham,distance_var,genome_ref_loc = compare_seqs(this_fasta[1][1][i:],this_fasta[2][1][i:],distance_metric,genome_ref_seq=this_fasta[0][1],read_frame=i)
-            print(df_row)
-            df.loc[df_row] = [f"{this_fasta[2][0]}_ORF+{i}",f"{this_fasta[1][0]}_ORF+{i}",insertion_count,deletion_count,substitution_count,length,distance_ham,distance_var,genome_ref_loc]
-            df_row += 1
-        consensus_id = this_fasta[1][0][1:]
-    for col in df.columns.to_list():
-        if 'ID' not in col:
-            plot_histo(df[col], outPath, col)
-            plotly_plot(df, outPath, col)
-    print(df.shape)
-    df_csv = f"{outPath}{consensus_id}_statsoutfile.csv"
-    df.to_csv(df_csv,index=False)
-    # df_xlsx = f"{outPath}{consensus_id}_statsoutfile.xlsx"
-    # df.to_excel(df_xlsx,index=False)      # need excelwriter like openpyxl 
+        sub_df = codon_subs(this_fasta[2],this_fasta[1],genome_ref_seq=this_fasta[0][1])
+        print(f"There are {sub_df.shape[0]} substitutions between {this_fasta[1][0]} and {this_fasta[2][0]}")
+        overall_dfs.append(sub_df)
+
+    df_csv = f"{outPath}substitutions_positions_file.csv"
+    overall = pd.concat(overall_dfs,sort=False)
+    overall.to_csv(df_csv,index=False)
 
 
 def main(argv):
@@ -266,7 +367,7 @@ def main(argv):
     elif file_end == 'fasta' and reading_frames is None:
         fasta_stats(file_list,outPath,distance_metric)
     else:
-        orf_stats(file_list,outPath,distance_metric)
+        codon_stats(file_list,outPath)
     
     
 
